ba7d34708c96371f24a0ef6ea3585b8293d5da6f
Alaa Al Naber <alnaberalaa@gmail.com>
2019-04-11 19:18:41 -0400

    Working removal of the div.extension-frame-wrapper selector.

--- scripts/injected.js
+++ scripts/injected.js
@@ -1,5 +1,35 @@
-console.log("Injected Flyover Blocker.");
+console.log("Injected Flyout Blocker.");
 
 window.onload = function() {
     console.log('Injected on load called.');
+    findElement('div.extension-frame-wrapper');
  };
+
+ 
+ function findElement(selector){ 
+    var element = document.querySelector(selector);
+    if(element == null){
+        console.log('Element with class name: ' + '(' + selector + ')' + ' not found.');
+    }else{
+        console.log('Element with class name: ' + '(' + selector + ')'+ ' found.');
+        element.parentElement.removeChild(element);
+    }
+ }
+
+function deleteElement(element){
+    element.remove();
+}
+
+// function waitForElementToDisplay(selector, time) {
+//     console.log(document.querySelector(selector));
+//     if(document.querySelector(selector)!=null) {
+//         alert("Element found exiting.")
+//         return;
+//     }
+//     else {
+//         setTimeout(function() {
+//             console.log('Cannot find.');
+//             waitForElementToDisplay(selector, time);
+//         }, time);
+//     }
+// }
